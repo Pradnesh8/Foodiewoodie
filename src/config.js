8ee852a9fcbe878a8e1265b285e6154f657a273f
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-05-04 00:14:11 +0530

    updated new API url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const REST_API_URL = "https://cors-anywhere-axpo.onrender.com/https://www.swiggy.com/dapi/restaurants/list/v5?page_type=DESKTOP_WEB_LISTING"
 export const REST_API_OFFSET_URL = "https://cors-anywhere-axpo.onrender.com/https://www.swiggy.com/dapi/restaurants/list/v5?sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
 export const REST_DATA_API_URL = "https://cors-anywhere-axpo.onrender.com/https://www.swiggy.com/dapi/menu/v4/full?menuId="
+export const REST_NEW_DATA_API_URL = "https://cors-anywhere-axpo.onrender.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true"//&lat=19.0160991&lng=73.0870326&restaurantId=66212
 export const CDN_IMG_URL = "https://res.cloudinary.com/swiggy/image/upload/"
 export const GIT_PROFILE_URL = "https://api.github.com/users/Pradnesh8"
 export const LINKED_IN_PROFILE_URL = "https://www.linkedin.com/in/pradnesh-khedekar-40180b148/"
